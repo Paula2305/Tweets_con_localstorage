80feba1a2d5c1eb7236f59d6e8dc2dc71013e690
Paula2305 <polimoreno1976@gmail.com>
2023-09-30 21:29:23 -0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+
+
+
+
+// Event listeners
+eventListeners();
+
+function eventListeners(){
+    formulario.addEventListener('submit',agregarTweet);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Funciones
+function agregarTweet(e){
+    e.preventDefault();
+
+    console.log('Agregando tweet');
+}
+
+
+
+
+
